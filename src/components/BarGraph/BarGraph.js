0900d83a8d39db6ff096ba98f4ae10b73f06af45
Lucas Nestor <lucas15nestor@gmail.com>
2020-02-09 13:13:21 -0500

    Remove unnecessary import

--- src/components/BarGraph/BarGraph.js
+++ src/components/BarGraph/BarGraph.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import {
-  BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, ResponsiveContainer, ReferenceLine
+  BarChart, Bar, XAxis, YAxis, CartesianGrid, ResponsiveContainer, ReferenceLine
 } from 'recharts';
 import './styles.css';
 
