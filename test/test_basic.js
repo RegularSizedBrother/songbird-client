1a530b095d3a158ef3e52fa7dff2edd60fdabee6
Elaina Liu <elainaliu15@gmail.com>
2020-02-08 22:00:49 -0500

    Selenium Test
    Moved selenium testing logic to another file, made the first test using selenium

--- test/test_basic.js
+++ test/test_basic.js
@@ -1,13 +1,5 @@
 var assert = require('assert');
 
-const webdriver = require('selenium-webdriver');
-
-const driver = new webdriver.Builder().forBrowser("firefox").build();
-
-driver.navigate().to("http://localhost:3000/");
-
-//driver.navigate().to("http://google.com");
-
 function add_two_nums (x, y){
     return x + y;
 }
